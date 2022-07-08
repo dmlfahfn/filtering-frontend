576b14126e6ee57f7588b5df153b0658d3acb6c2
dmlfahfn <dmlfahfn1@hotmail.se>
2022-07-09 01:54:42 +0200

    still stuck

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,22 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Filter = (props) => {
+
+    const [isSubscribed, setIsSubscribed] = useState(false);
 
     const uniqueProducts = [...new Set(props.videos.map(item => item.product))];
     const uniqueVersions = [...new Set(props.videos.map(item => item.version))];
 
     const handleOnChange = (e) => {
-        props.videos.filter(product => {
-            if(e.target.name === product.product){
-                product.isChecked = true;  
-                if(!props.filteredArray.includes(product)){
-                    console.log(props.filteredArray.hasOwnProperty(product));
-                    props.setFilteredArray(current =>[...current, product])
-                    console.log(props.filteredArray)
+        // props.videos.filter(product => {
+        //     if(e.target.name === product.product){
+        //         product.isChecked = true;  
+        //         if(!props.filteredArray.includes(product)){
+        //             console.log(props.filteredArray.hasOwnProperty(product));
+        //             props.setFilteredArray(current =>[...current, product])
+        //             console.log(props.filteredArray)
+        //         }
+        //     }
+        //     return props.filteredArray
+        // })
+
+
+            if (e.target.checked) {
+                console.log('✅ Checkbox is checked');
+                props.videos.filter(product => {
+                if(e.target.name === product.product){
+                    if(props.filteredArray.hasOwnProperty(product)){
+                        props.setFilteredArray(current =>[...current, product])
+                    }
                 }
-            }
-            return props.filteredArray
-        })
+               else {
+                console.log('⛔️ Checkbox is NOT checked');}
+            })
+              }
+              setIsSubscribed(current => !current);
+              return props.filteredArray
+        
+        
+
         
     }
   
@@ -25,7 +46,7 @@ const Filter = (props) => {
             <h4>Products</h4>
             {uniqueProducts.map(filter => (
                 <div key={filter}>
-                    <input type="checkbox" name={filter} onChange={handleOnChange}/> {filter}
+                    <input type="checkbox" name={filter} value={isSubscribed} onChange={handleOnChange}/> {filter}
                 </div>
             ))}
             <h4>Version</h4>
