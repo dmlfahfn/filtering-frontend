6af8579fe71917d40d71923beb0550acdf4e2ff1
dmlfahfn <dmlfahfn1@hotmail.se>
2022-07-13 00:13:14 +0200

    wokring with backend

--- src/App.js
+++ src/App.js
@@ -2,53 +2,30 @@ import './App.css';
 import Filter from "./components/Filter";
 import VideoDisplay from "./components/VideoDisplay";
 import Search from "./components/Search";
-import img1 from "./img/img_avatar.png";
-import img2 from "./img/img_avatar2.png";
-import { useState } from 'react';
+// import img1 from "./img/img_avatar.png";
+// import img2 from "./img/img_avatar2.png";
+import { useState, useEffect } from 'react';
 
 function App() {
 
-  const videos = [
-    {
-    id: 1,
-    image : img1,
-    title: "Loading data from catalog",
-    description: "This is some description text...",
-    product: "Qlik catalog",
-    version: "November 2021",
-    checked: false
-  },
-  {
-    id: 2,
-    image : img1,
-    title: "Get to know the loop block",
-    description: "This is some description text...",
-    product: "Qlik catalog",
-    version: "November 2021",
-    checked: false
-  },
-  {
-    id: 3,
-    image : img2,
-    title: "This is another TITLE!!!",
-    description: "This is some description text...",
-    product: "QlikView",
-    version: "December 2021",
-    checked: false
-  },
-  {
-    id: 4,
-    image : img2,
-    title: "Another title",
-    description: "This is some description text.",
-    product: "Alerting",
-    version: "October 2021",
-    checked: false
-  }
-]
-
-const [filteredArray, setFilteredArray] = useState([]) 
+const [videos, setVideos] = useState([]);
+const [filteredArray, setFilteredArray] = useState([]); 
 const [filteredResults, setFilteredResults] = useState([]);
+
+useEffect(() => {
+  const url = "http://localhost:3000/videos"
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      setVideos(json);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  fetchData();
+}, []);
 
 
   return (
